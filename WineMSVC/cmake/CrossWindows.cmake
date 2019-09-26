7ff9357e350fcc7521088ae5199ac6717dc6e937
Wohlstand <admin@wohlnet.ru>
2019-09-26 15:29:08 +0300

    Specify compiler inside toolchain file

--- WineMSVC/cmake/CrossWindows.cmake
+++ WineMSVC/cmake/CrossWindows.cmake
@@ -3,5 +3,11 @@ set(CMAKE_SYSTEM_VERSION 6.1.7601)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_CROSSCOMPILING 1)
 
+set(CMAKE_C_COMPILER wx-cl)
+set(CMAKE_CXX_COMPILER wx-cl)
+set(CMAKE_LINKER wx-link)
+set(CMAKE_MT wx-mt)
+set(CMAKE_RC_COMPILER wx-rc)
+
 include(Platform/Windows)
 
