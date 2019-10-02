e0fc8df65f14466d871c4ef879ad6f1480d058e8
Wohlstand <admin@wohlnet.ru>
2019-10-02 15:02:52 +0300

    Stabilize the workflow

--- WineMSVC/bin/_msvc_wine_wrapper_common.sh
+++ WineMSVC/bin/_msvc_wine_wrapper_common.sh
@@ -1,45 +1,74 @@
 
+export WINEDEBUG=-all
+
+function isPath()
+{
+    if [[ (-f "$1" || -d "$1") ]]; then
+        echo 1
+    fi
+}
+
 function unixToWin()
 {
     val=$1
-    if [[ "${val::1}" == "/" ]]; then
-        val="`winepath --windows \"${val}\"`"
+    if [[ "${val}" == "" ]]; then
+        val="" #Change nothing
+    elif [[ "${val::1}" == "/" ]]; then
+        val="`winepath --windows \"${val}\"`" # Convert absolute path
     else
-        val="${val//\//\\\\}"
+        val="${val//\//\\\\}" # Convert relative path
     fi
     echo -n "${val}"
 }
 
 ORIG_ARGS=$@
 MSVC_TO_ARGS=
+
 for var in "$@"
 do
+    key=
+    val=$var
+
     # Convert link and include paths -I/path/to, -L/path/to, etc.
-    if [[ ("${var::2}" == "-I" || "${var::2}" == "-L") && (-d "${var:2}" || -f "${var:2}") ]]; then
+    if [[ ("${var::2}" == "-I" || "${var::2}" == "/I" || "${var::2}" == "-L" || "${var::2}" == "/L") && $(isPath "${var:2}") ]]; then
         key=${var::2}
         val="$(unixToWin "${var:2}")"
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} ${key}${val}"
+    # Convert "/Fe:path/to/some" argument
+    elif [[ "${var::4}" == "/Fe:" ]]; then
+        key=${var::4}
+        val="$(unixToWin "${var:4}")"
     # Convert paths of /Fo, Fx, etc. /Fo/path/to, /Fx/path/to, etc.
-    elif [[ ("${var::2}" == "/F") && (-d "${var:3}" || -f "${var:3}") ]]; then
+    elif [[ "${var::3}" == "/Fa" || "${var::3}" == "/Fd" || "${var::3}" == "/Fe" || "${var::3}" == "/Fi" || \
+            "${var::3}" == "/Fm" || "${var::3}" == "/Fo" || "${var::3}" == "/Fp" || \
+            "${var::3}" == "/FR" || "${var::3}" == "/Fr" || \
+            "${var::3}" == "/AI" || "${var::3}" == "/FI" ]]; then
         key=${var::3}
         val="$(unixToWin "${var:3}")"
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} ${key}${val}"
-    # Convert linker's /out path: /out:kek/path
-    elif [[ "${var::5}" == "/out:" || "${var::5}" == "/OUT:" ]]; then
+    # Convert linker's /out path: "/out:kek/path"
+    elif [[ "${var::5}" == "/out:" || "${var::5}" == "/OUT:" || "${var::5}" == "/pdb:" || "${var::5}" == "/PDB:" ]]; then
         key=${var::5}
         val="$(unixToWin "${var:5}")"
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} ${key}${val}"
+    # Convert linker's implib path: "/implib:kek/path"
+    elif [[ "${var::8}" == "/implib:" || "${var::8}" == "/IMPLIB:" ]]; then
+        key=${var::8}
+        val="$(unixToWin "${var:8}")"
     # Convert path to manifest file: /MANIFESTFILE:/path/to, etc.
     elif [[ "${var::14}" == "/MANIFESTFILE:" ]]; then
         key=${var::14}
         val="$(unixToWin "${var:14}")"
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} ${key}${val}"
+    # Convert "/outputresource:" path
+    elif [[ "${var::16}" == "/outputresource:" || "${var::16}" == "/OUTPUTRESOURCE:" ]]; then
+        key=${var::16}
+        val="$(unixToWin "${var:16}")"
+    # Convert path to manifest file: -LIBPATH:/path/to, etc.
+    elif [[ "${var::9}" == "-LIBPATH:" ]]; then
+        key=${var::9}
+        val="$(unixToWin "${var:9}")"
     # Convert absolute or relative path arguments
     elif [[ ("${var}" != "/lib") && ("${var}" != "/bin") && (-d "$var" || -f "$var") ]]; then
         val="$(unixToWin "${var}")"
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} ${val}"
-    # Pass any unknown arguments as-is
-    else
-        MSVC_TO_ARGS="${MSVC_TO_ARGS} $var"
-    fi
+    fi # Anything other, pass as-is
+
+    MSVC_TO_ARGS="${MSVC_TO_ARGS} ${key}${val}"
 done
+
